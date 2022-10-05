66ba9b73f251be333a2badd50dd4cf2dbce7bbe0
Kaleb <63081938+Kaleb47@users.noreply.github.com>
2022-10-05 15:14:03 -0400

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,12 @@
+# use hash map to instantly check for difference value, map will add index of last occurrence of a num, don’t use same element twice;
+# two sum
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} # val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
