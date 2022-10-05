bce8f49aa41100275e44a9f1f729c214e0b6aa51
Kaleb <63081938+Kaleb47@users.noreply.github.com>
2022-10-05 15:13:25 -0400

    Update validAnagram.py

--- validAnagram.py
+++ validAnagram.py
@@ -1,1 +1,10 @@
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap = map()
+        
+        for s and t in self:
+            if s and t in hashmap:
+                return True
+            hashmap.add(s, t)
+        return False
